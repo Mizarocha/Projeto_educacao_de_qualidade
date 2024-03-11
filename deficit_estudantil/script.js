34e2d36db9bd18d8e08e38a113df2d414a1fcfee
Artemiza Rocha <miza-pma@hotmail.com>
2024-03-11 17:14:36 -0300

    alterando o htm, css e js

--- deficit_estudantil/script.js
+++ deficit_estudantil/script.js
@@ -1,7 +1,7 @@
-function mostrarAba(id) {
+function mostrarAba(evt) {
   var abas = document.querySelectorAll(".grid-section");
   for (var i = 0; i < abas.length; i++) {
-    if (abas[i].id === id) {
+    if (abas[i].id === evt) {
       abas[i].style.display = "block";
     } else {
       abas[i].style.display = "none";

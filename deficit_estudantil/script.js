1a9abf84c433a5aaff06dbcf2c25407d9aa6761a
Artemiza Rocha <miza-pma@hotmail.com>
2024-04-13 21:30:41 -0300

    criando grafico no js

--- deficit_estudantil/script.js
+++ deficit_estudantil/script.js
@@ -12,3 +12,79 @@ function mostrarAba(evt) {
 function iniciarFormulario() {
   mostrarAba();
 }
+
+const ctx = document.getElementById("grafico1");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["2024", "2023", "2022", "2021", "2020", "2019"],
+    datasets: [
+      {
+        label: "Maior Porcentagem",
+        backgroundColor: [
+          "rgb(255, 205, 86)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 99, 132)",
+        ],
+        data: [100, 30, 50, 80, 40, 20],
+        borderWidth: 1,
+        borderColor: "#000",
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "Educação de Qualidade",
+        color: "#fff",
+        font: {
+          size: 18,
+        },
+      },
+    },
+  },
+});
+
+const novo = document.getElementById("grafico2");
+
+const data = {
+  labels: ["São Paulo", "Belo Horizonte", "Pernambuco"],
+  datasets: [
+    {
+      label:
+        "Cidades com maior índice de desempenho de educação com Construindo Sonhos",
+      data: [300, 50, 100],
+      backgroundColor: [
+        "rgb(0, 200, 0)",
+        "rgb(255, 20, 147)",
+        "rgb(255, 255, 0)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const chart = new Chart(novo, {
+  type: "doughnut",
+  data: data,
+  labels: ["Rio de Janeiro", "Belo Horizonte", "Pernambuco"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [300, 50, 100],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+});
